4a6fe0aaa3f703eb84ba94237b1f0da00524f6c7
Angel Callaspo <callaspoangel@gmail.com>
2024-07-27 16:04:48 -0300

    corrigindo arquivos pylint

--- src/duplicado.py
+++ src/duplicado.py
@@ -6,7 +6,6 @@ def df_duplicado (df):
     tam_df_duplicado = df.duplicated().sum()
     if tam_df_duplicado > 0:
         return print(f"{aux_df_duplicado}, Quantidade de duplicados explicitos é = {tam_df_duplicado}")
-       
     else:
         return print(f"Quantidade de duplicados explicitos na tabela é = {tam_df_duplicado}")
 

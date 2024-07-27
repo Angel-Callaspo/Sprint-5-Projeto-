dd6209a98fd44715830bbad6ba011e6c2197f50d
Angel Callaspo <callaspoangel@gmail.com>
2024-07-27 19:14:24 -0300

    atualizando arquivo

---
+++ src/titulo.py
@@ -0,0 +1,10 @@
+def cabeçalhos_df (df):
+    """Corrigir cabeçalhos de colunas"""
+    coluna_nova = []
+    for aux_col in df.columns:
+        sem_espaços = aux_col.strip()
+        minuscula = sem_espaços.lower()
+        underline = minuscula.replace(' ','_')
+        coluna_nova.append(underline)
+    df.columns = coluna_nova
+    return df

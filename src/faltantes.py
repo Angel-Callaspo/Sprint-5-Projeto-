111bc5a5ce2b317a0309dffb1ea5f9f47e39694f
Angel Callaspo <callaspoangel@gmail.com>
2024-07-28 10:52:48 -0300

    Atualizando arquivo faltantes.py função

--- src/faltantes.py
+++ src/faltantes.py
@@ -1,3 +1,8 @@
+def faltante_categorico(data_frame, column, valor):
+    """Preenche ausentes/ condicional NAN deve ser = '' por keep_default_na=False na letura"""
+    data_frame[column]=data_frame[column].replace('', valor)
+    return print(f"Valores univocos da coluna: {column}, são: {data_frame[column].unique()}")
+
 def substituir_nan_coluna(df,column,valor):
     """substituir valores faltantes em uma coluna"""
     for col in column:

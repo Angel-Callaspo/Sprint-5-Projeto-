4e92458ee039179dc5aefbb11a601827d5ab407b
Angel Callaspo <callaspoangel@gmail.com>
2024-07-27 21:55:33 -0300

    atualizando arquivos

--- src/conv_float_int.py
+++ src/conv_float_int.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 def conver_float_int(data, column):
+    """Conversão de tipo float para inteiro"""
     verifica_conver = np.array_equal(data[column], data[column].astype('int'))
-    if verifica_conver == True:
+    if verifica_conver==True:
         data[column] = data[column].astype('int')
         print(data.info(show_counts=True))
         return print("É seguro a conversão de float64 para int64")

a94770d182fcb381c99f40ca63f42cf06098bafa
Angel Callaspo <callaspoangel@gmail.com>
2024-07-28 11:48:29 -0300

    Editando arquivo .py conv_dado_num.py

--- src/conv_dado_num.py
+++ src/conv_dado_num.py
@@ -9,3 +9,9 @@ def conver_float_int(data, column):
         return print("É seguro a conversão de float64 para int64")
     else:
         return print('Não é seguro a conversão de tipos de dados de float64 para int64')
+
+
+def conver_str_float(data, column):
+    """Conversão de tipo float para inteir"""
+    data[column] = data[column].astype('int')
+    return print(data[column].info())

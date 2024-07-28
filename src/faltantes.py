3808b962c9f01922bf40e7b5d95ade5af921cd91
Angel Callaspo <callaspoangel@gmail.com>
2024-07-27 22:06:18 -0300

    Atualizando arquivos

--- src/faltantes.py
+++ src/faltantes.py
@@ -1,5 +1,5 @@
-def substituir_nan_coluna (df,column,valor):
+def substituir_nan_coluna(df,column,valor):
     """substituir valores faltantes em uma coluna"""
-    for col in column:
+    for col in df[column]:
         df[col].fillna(valor, inplace=True)
     return df

11421759c49cf511f5059f4b2277df94fb1acd34
Angel Callaspo <callaspoangel@gmail.com>
2024-07-29 17:19:04 -0300

    Atualizando projeto

--- src/conv_dado_num.py
+++ src/conv_dado_num.py
@@ -1,17 +1,27 @@
 import numpy as np
 
-def conver_float_int(data, column):
-    """Conversão de tipo float para inteiro"""
+def seg_float_int(data, column):
+    """Conversão segura de tipo float para inteiro"""
     verifica_conver = np.array_equal(data[column], data[column].astype('int'))
     if verifica_conver==True:
+        #print(data[column].info(show_counts=True))
         data[column] = data[column].astype('int')
-        print(data.info(show_counts=True))
-        return print("É seguro a conversão de float64 para int64")
+        print(data[column])
     else:
         return print('Não é seguro a conversão de tipos de dados de float64 para int64')
 
-
 def conver_str_float(data, column):
-    """Conversão de tipo float para inteir"""
+    """Conversão de tipo string para float"""
+    data[column] = data[column].astype('float')
+    return print(data[column].info())
+
+def conv_float_int(data, column):
+    """Conversão de tipo string para float"""
+    data[column] = data[column].astype('int')
+    return print(data[column].info())
+
+def conv_str_int(data, column):
+    """Conversão de tipo string para int"""
+    data[column] = data[column].astype('float')
     data[column] = data[column].astype('int')
     return print(data[column].info())

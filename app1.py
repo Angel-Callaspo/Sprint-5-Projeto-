ef9f289a03058bb3179d349dd7d20ebbbeb01f42
Angel Callaspo <callaspoangel@gmail.com>
2024-07-30 10:35:25 -0300

    Atualizando arquivo app1.py

--- app1.py
+++ app1.py
@@ -4,6 +4,14 @@ import streamlit as st
 from pathlib import Path
 
 df_carro = pd.read_csv(Path("data/vehicles.csv")) # lendo dados
+hist_button = st.button('Criar histograma') # criar um botão
 
-fig = px.histogram(df_carro, x= 'odometer')
-st.plotly_chart(fig)
+if hist_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    
+    # criar um histograma
+    fig = px.histogram(car_data, x="odometer")
+     
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)

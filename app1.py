9902dfd20f84e161b7b27e2db23a105b5c1befcb
Angel Callaspo <callaspoangel@gmail.com>
2024-07-30 10:13:08 -0300

    atualizando arquivo

--- app1.py
+++ app1.py
@@ -6,6 +6,4 @@ from pathlib import Path
 df_carro = pd.read_csv(Path("data/vehicles.csv")) # lendo dados
 
 fig = px.histogram(df_carro, x= 'odometer')
-
-st.title('Primeiro dashboard')
-st.plotly_chart(fig) #use_container_width=True
+st.plotly_chart(fig)

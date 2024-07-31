63592dfc0b6cff51b3a75e15832e4ce70772ce2f
Angel Callaspo <callaspoangel@gmail.com>
2024-07-31 17:54:35 -0300

    atualizando arquivos

--- app1.py
+++ app1.py
@@ -8,11 +8,11 @@ st.title('Dashboard Anuncios de Carros')
 criar_caixa_mod_qua = st.checkbox('Modelos Anunciados') # criar uma caixa histograma
 criar_caixa_mod_pre = st.checkbox('Modelos vs Preços Médios') # criar caixa de seleção
 criar_caixa_pre_hod = st.checkbox('Preços vs hodômetro') # criar caixa de seleção
-criar_caixa_modelo = st.checkbox('Modelos: informação integrada') # criar caixa de seleção
+criar_bot_filtro = st.button('Filtra dados por modelos') # criar botão filtrar base dados
 
 if criar_caixa_mod_qua: # se o botão for clicado
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
-    fig = px.histogram(df_carro, x="model", color='red') # criar um histograma
+    fig = px.histogram(df_carro, x="model") # criar um histograma
     st.title('Quantidade de Modelos de carros anunciados')
     fig.show()
     st.plotly_chart(fig, use_container_width=True) # exibir um gráfico Plotly interativo
@@ -39,3 +39,17 @@ if criar_caixa_pre_hod:
     if criar_botao: # se botão for clicado
         resumo = df_carro.loc[:,['odometer', 'price']]
         st.dataframe(resumo)
+
+
+col1 = st.text_input('inserir modelo')
+botao_executar = st.button('Filtra')
+st.write('Banco de dados de Anuncios de Carros')
+st.dataframe(df_carro)
+if botao_executar:
+    filtro = df_carro[df_carro['model']==col1]
+    if len(filtro) == 0:
+        st.write(f"O modelo '{col1}', Não está registrado")
+    else:
+        st.write(f'Relatorio para o modelo: {col1}')
+        st.dataframe(filtro)
+#on_select
